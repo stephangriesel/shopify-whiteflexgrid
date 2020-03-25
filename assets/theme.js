7293dc77b907e932eac11e20318afde9cff55f69
Stephan <sgriesel@gmail.com>
2020-03-25 07:12:20 +0100

    faq, styles & js, schema not working

--- assets/theme.js
+++ assets/theme.js
@@ -100,3 +100,20 @@ $('.product-category select').on('change', function () {
     updateHistoryState(found)
     $('#variant-id').val(found.id)
 });
+
+/* Accordion */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
