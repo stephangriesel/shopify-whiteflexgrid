d61209d2dab0e84c9d1e27b716a4f8c4b3a1d45d
Stephan <sgriesel@gmail.com>
2020-03-29 12:07:34 +0200

    add & fix js

--- assets/theme.js
+++ assets/theme.js
@@ -14,3 +14,44 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+// Get form data
+
+function getVariantFromOptions() {
+    let variantArr = []
+    $(".product-category select").map(function (i, el) {
+        variant = { value: $(el).val(), index: $(el).data('index') };
+        variantArr.push(variant)
+    });
+    return variantArr;
+}
+
+function updateHistoryState(variant) {
+    if (!history.replaceState || !variant) {
+        return;
+    }
+
+    var newurl =
+        window.location.protocol +
+        '//' +
+        window.location.host +
+        window.location.pathname +
+        '?variant=' +
+        variant.id;
+
+    window.history.replaceState({ path: newurl }, '', newurl);
+}
+
+$('.product-category select').on('change', function () {
+    var selectedValues = getVariantFromOptions();
+    var variants = window.product.variants;
+
+    // Search for product variants based on what was selected in the dropdowns
+    var found = _.find(variants, function (variant) {
+        return selectedValues.every(function (values) {
+            return _.isEqual(variant[values.index], values.value);
+        });
+    });
+    updateHistoryState(found)
+    $('#variant-id').val(found.id)
+});
